6815c8558100d5dc223120d7d2f99fcca648bd84
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-02 14:33:12 +0100

    Inizializzazione laravel

---
+++ laravel-model-contrller/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('shared.app')
+
+@section('sectionHome')
+home
+@endsection
---
+++ laravel-model-contrller/resources/views/shared/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Laravel Model Controller</title>
+
+   <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    @yield('sectionHome')
+</body>
+</html>
