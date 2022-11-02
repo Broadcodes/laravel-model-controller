147861a9075c06ba296bacd797a8cc6870e4310e
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-02 21:13:46 +0100

    Bonus

--- laravel-model-contrller/resources/views/home.blade.php
+++ laravel-model-contrller/resources/views/home.blade.php
@@ -1,9 +1,18 @@
 @extends('shared.app')
 
 @section('sectionHome')
-@foreach ($movies as $item)
-    <div>
-        {{$item}}
+    <div class="container">
+        <h1>Data Movies in Database</h1>
+        <div class="cards">
+            @foreach ($movies as $item)
+                <div class="card">
+                    <h2>{{ $item['title'] }}</h2>
+                    <h3>{{ $item['original_title'] }}</h3>
+                    <h3>{{ $item['nationality'] }}</h3>
+                    <h3>{{ $item['date'] }}</h3>
+                    <h3>Vote: {{ $item['vote'] }}</h3>
+                </div>
+            @endforeach
+        </div>
     </div>
-@endforeach
 @endsection
